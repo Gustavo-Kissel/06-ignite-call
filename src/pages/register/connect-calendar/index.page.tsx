0a261c517df90f56c4b8cb3dc57f6a764c2b514f
GustavoKissel <kisselgustavo@gmail.com>
2023-10-23 13:46:22 -0300

    Calendar Permission and Error Handling

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,13 +1,21 @@
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
-import { ArrowRight } from 'phosphor-react'
+import { ArrowRight, Check } from 'phosphor-react'
 // import { api } from "../../../lib/axios"
 import { Container, Header } from '../styles'
-import { ConnectBox, ConnectItem } from './styles'
+import { AuthError, ConnectBox, ConnectItem } from './styles'
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 export default function Register() {
-  // async function handleRegister() {
+  const session = useSession()
+  const router = useRouter()
 
-  // }
+  const hasAuthError = !!router.query.error
+  const hasSignIn = session.status === 'authenticated'
+
+  async function handleSignIn() {
+    await signIn('google')
+  }
 
   return (
     <Container>
@@ -24,13 +32,27 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" size="sm">
-            Conectar
-            <ArrowRight />
-          </Button>
+          {hasSignIn ? (
+            <Button size="sm" disabled>
+              Conectado
+              <Check />
+            </Button>
+          ) : (
+            <Button variant="secondary" size="sm" onClick={handleSignIn}>
+              Conectar
+              <ArrowRight />
+            </Button>
+          )}
         </ConnectItem>
 
-        <Button type="submit">
+        {hasAuthError && (
+          <AuthError size="sm">
+            Falha ao se conectar ao Google, verifique se você habilitou as
+            permissões da acesso ao Google Calendar.
+          </AuthError>
+        )}
+
+        <Button type="submit" disabled={!hasSignIn}>
           Próximo passo
           <ArrowRight />
         </Button>
